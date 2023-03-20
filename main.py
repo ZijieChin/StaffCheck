25cf8c6926d563c5cc1bc1bfdb27112dbf8b4ba7
qzj995@outlook.com <qzj995@outlook.com>
2023-03-20 16:57:08 +0800

    update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from reader import read_excel_to_sqlite, clear_tables, export_sqlite_to_excel
 from funcs import simple_match, complex_match
-from conf import filepath
+from conf import filepath, outputpath
 
 
 def main():
@@ -10,7 +10,7 @@ def main():
     simple_match()
     complex_match()
 
-    export_sqlite_to_excel(filepath)
+    export_sqlite_to_excel(outputpath)
 
 
 if __name__ == "__main__":
